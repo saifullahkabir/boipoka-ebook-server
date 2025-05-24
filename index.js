e82a92604f5c536c4f7385cc430c9b9f5f327caa
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-05-25 01:33:49 +0600

    implement auth related api and logout

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 // Verify Token Middleware
 const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token
-  console.log(token)
+  console.log(token);
   if (!token) {
     return res.status(401).send({ message: 'unauthorized access' })
   }
@@ -71,6 +71,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const booksCollection = client.db('boipoka-ebook').collection('books');
+
+    // auth related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+
+    // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+        console.log('Logout successful')
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
 
     // Upload Book
     app.post('/books', upload.single('pdf'), async (req, res) => {
